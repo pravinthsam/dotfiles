02d11c1c93d9649a7d4927e3ac916a491053a895
Pravinth Samuel Vethanayagam <pravinth.samuel@gmail.com>
2018-07-09 16:27:45 -0400

    Disable lockscreen when a window is full screen.

--- scripts/lockscreen.sh
+++ scripts/lockscreen.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
-revert() {
-  rm /tmp/*screen*.png
-  xset dpms 0 0 0
-}
-trap revert HUP INT TERM
-xset +dpms dpms 0 0 5
-scrot -d 1 /tmp/locking_screen.png
-convert -blur 0x8 /tmp/locking_screen.png /tmp/screen_blur.png
-convert -composite /tmp/screen_blur.png /home/pravinth/scripts/rick.png -gravity South -geometry -20x1200 /tmp/screen.png
-i3lock -i /tmp/screen.png
-revert
+
+num_full=$(python -c "import i3ipc; i3 = i3ipc.Connection(); print(len(i3.get_tree().find_fullscreen()))")
+
+if [ $num_full -eq "0" ]; then
+	revert() {
+	  rm /tmp/*screen*.png
+	  xset dpms 0 0 0
+	}
+	trap revert HUP INT TERM
+	xset +dpms dpms 0 0 5
+	scrot -d 1 /tmp/locking_screen.png
+	convert -blur 0x8 /tmp/locking_screen.png /tmp/screen_blur.png
+	convert -composite /tmp/screen_blur.png /home/pravinth/scripts/rick.png -gravity South -geometry -20x1200 /tmp/screen.png
+	i3lock -i /tmp/screen.png
+	revert
+else
+	xautolock -restart
+fi
